8381480fd67e4726439bb3b9307095f4d3d80836
Nathan O'Hanlon <nathan@eit.co.nz>
2019-03-12 12:00:46 +1300

    modify xero-php reference.

--- src/XeroPHP/Remote/RemoteObject.php
+++ src/XeroPHP/Remote/RemoteObject.php
@@ -5,6 +5,6 @@ namespace XeroPHP\Remote;
 /**
  * @deprecated As of PHP 7.2 Object is a reserved word, use XeroPHP\Remote\Model instead.
  */
-abstract class Object extends Model
+abstract class RemoteObject extends Model
 {
 }
